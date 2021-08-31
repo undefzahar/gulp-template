e9135a37fe6df3266b5cbbf619336cc880e7a3eb
undefzahar <undef.zahar@gmail.com>
2021-08-31 16:33:46 +0300

    first commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<title><?php wp_title(); ?></title>
+
+	<?php wp_head(); ?>
+
+
+</head>
+
+<body>
+
+
+	<main <?php body_class(); ?>>
+
+		<header class="header">
+		
+		</header>
+		<div class='main-wrap'>
